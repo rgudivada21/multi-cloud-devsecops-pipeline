18dcc16c47a6bf1aee44f38aaf81667178af6416
rgudivada21 <rgudivada21@gmail.com>
2026-07-25 09:37:36 +0530

    Create outputs.tf
    Complete GCP Terraform configuration

---
+++ terraform/gcp/outputs.tf
@@ -0,0 +1,9 @@
+output "gcp_project" {
+  description = "Configured GCP Project"
+  value       = var.gcp_project
+}
+
+output "gcp_region" {
+  description = "Configured GCP Region"
+  value       = var.gcp_region
+}

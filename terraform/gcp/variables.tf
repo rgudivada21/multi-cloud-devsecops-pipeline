8a9093a287fa1c02a76f76b3d59fa9fb44fb7fe4
rgudivada21 <rgudivada21@gmail.com>
2026-07-25 09:37:01 +0530

    Create variables.tf
    add variables

---
+++ terraform/gcp/variables.tf
@@ -0,0 +1,11 @@
+variable "gcp_project" {
+  description = "GCP Project ID"
+  type        = string
+  default     = "my-gcp-project"
+}
+
+variable "gcp_region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
